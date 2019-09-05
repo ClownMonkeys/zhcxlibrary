5b3d6888c26898060e3986207987956b812d107a
qudapeng <dapeng_qu@123cx.com>
2019-09-05 10:25:01 +0800

    Initial commit

---
+++ commonlib/src/main/java/com/zhcx/commonlib/StrUtils.kt
@@ -0,0 +1,18 @@
+package com.zhcx.commonlib
+
+/**
+ * ProjectName: zhcxlibrary
+ * PackageName: com.zhcx.commonlib
+ * description:
+ * Date：2019/9/5 10:18
+ * Author：Steven J
+ * ClassName:StrUtils
+ * Modify The Note:
+ **/
+class StrUtils {
+    companion object{
+        fun pri(): String {
+            return "this is lib"
+        }
+    }
+}
